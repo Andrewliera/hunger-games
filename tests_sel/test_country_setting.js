e572898ef248320f5bcff85610fea2b9efed2763
andrew <andrewliera15@gmail.com>
2023-04-11 20:05:34 -0400

    added selenium to project, added test to check basic functionality of fix for issue #388

---
+++ tests_sel/test_country_setting.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+const {Builder,By, Key} = require ("selenium-webdriver");
+
+async function test_drop_down(){
+  
+  //launch browser
+  let driver = await new Builder().forBrowser('firefox').build();
+
+  //navigate to app
+  await driver.get('http://localhost:3000/')
+
+  //exit opening message (using escape wasnt working)
+  const hoverable = driver.findElement(By.className('sc-beqWaB heWUWm sc-hLseeU eCViBy reactour__close'));
+  const actions = driver.actions({async: true}); 
+  await actions.move({origin: hoverable}).click().perform();
+  
+  //go to settings
+  await driver.get('http://localhost:3000/settings');
+
+  //hit the country drop down
+  await driver.findElement(By.id(":r5:")).click();
+  
+  //press tab and press enter 
+  await driver.actions().sendKeys(Key.ESCAPE).perform();
+  await driver.actions().sendKeys(Key.ENTER, Key.ENTER).perform();
+  
+  
+  //get the value
+  const label = await driver.findElement(By.id(":r5:")).getText();
+  
+  //
+  const test = "Belgium";
+  assert(test == label);
+
+}
+test_drop_down();
